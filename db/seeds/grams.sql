571585c10e200348b89bef5dc98cbeaedcae7f31
Aileen Wong <aileen.m.wong@gmail.com>
2017-09-01 21:56:27 -0400

    adds some css styling to all pages

--- db/seeds/grams.sql
+++ db/seeds/grams.sql
@@ -15,77 +15,77 @@ INSERT INTO phototypes (imgtype) VALUES
 
 INSERT INTO grams (photo, location, status, comments, type) VALUES
   (
-    'http://i.imgur.com/rslZjPjb.jpg',
+    'http://i.imgur.com/rslZjPj.jpg',
     'Taiyaki',
     'did it',
     'fish ice cream',
     '1'
   ),
   (
-    'http://i.imgur.com/xJ4lQIqb.jpg',
+    'http://i.imgur.com/xJ4lQIq.jpg',
     'Los Molcajetes',
     'gotta go',
     'flaming queso',
     '2'
   ),
   (
-    'http://i.imgur.com/e4jsno8b.jpg',
+    'http://i.imgur.com/e4jsno8.jpg',
     'Scooper Bowl',
     'did it',
     'All you can eat ice cream',
     '4'
   ),
   (
-    'http://i.imgur.com/SxxOZcUb.jpg',
+    'http://i.imgur.com/SxxOZcU.jpg',
     'Deauville Inn',
     'gotta go',
     'Tornado Fries',
     '2'
   ),
   (
-    'http://i.imgur.com/yCeK7IOb.jpg',
+    'http://i.imgur.com/yCeK7IO.jpg',
     'New World Mall',
     'gotta go',
     'Bowl of Meat',
     '2'
   ),
   (
-    'http://i.imgur.com/KBnQIMyb.jpg',
+    'http://i.imgur.com/KBnQIMy.jpg',
     'Snowdays',
     'did it',
     'Snow cream topped with Frosted Flakes',
     '1'
   ),
   (
-    'http://i.imgur.com/Cx9mFTeb.jpg',
+    'http://i.imgur.com/Cx9mFTe.jpg',
     'Loving Tea',
     'did it',
     'light bulb tea',
     '1'
   ),
   (
-    'http://i.imgur.com/sBu9ykCb.jpg',
+    'http://i.imgur.com/sBu9ykC.jpg',
     'Jersey Shore',
     'gotta go',
     'bar on the beach',
     '3'
   ),
   (
-    'http://i.imgur.com/yfvfMrFb.jpg',
+    'http://i.imgur.com/yfvfMrF.jpg',
     'Black Tap',
     'gotta go',
     'freakshake',
     '1'
   ),
   (
-    'http://i.imgur.com/sD7RJBEb.jpg',
+    'http://i.imgur.com/sD7RJBE.jpg',
     'Maritas',
     'did it',
     'cotton candy ice cream',
     '1'
   ),
   (
-    'http://i.imgur.com/MdZeC2Kb.jpg',
+    'http://i.imgur.com/MdZeC2K.jpg',
     'Milk & Cream',
     'gotta go',
     'cereal ice cream',
