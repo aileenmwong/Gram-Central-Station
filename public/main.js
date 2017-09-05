b5ac7fafc64ac4f4c783c14518ece724d0b7850c
Aileen Wong <aileen.m.wong@gmail.com>
2017-09-05 10:22:38 -0400

    add more comments

--- public/main.js
+++ public/main.js
@@ -20,43 +20,51 @@ counter = 0;
       .done(function(data) {
         console.log('the data stuff ->>', data);
         // debugger
+        // loop through the data to grab data from API to render to the page
         for (let i=0; i < data.response.venues.length; i++) {
-
         location = data.response.venues[i].name;
         address = data.response.venues[i].location.formattedAddress;
         phone = data.response.venues[i].contact.formattedPhone;
         website = data.response.venues[i].url;
-
+        //manipulate the dom to render information dynamically
         manipulateDom(location,address,phone,website);
       }
       })
       .fail(function(data) {
         console.log('failed getting locations');
       })
+      //increase the counter to make the next item
       counter ++;
   }
 
   // change the inner html of lis with appropriate data
   var manipulateDom = function(location, address, phone, website, type) {
     console.log('manipulating the dom',location,address[0],phone,website);
-
+    //create a container to append the lis
     let $container = $('<ul>').attr('class', 'resultCont');
 
     let locationItem = $('<li>').attr('class', 'searchLocation').html(location);
+    //append the location to the container
     locationItem.appendTo($container);
 
     let addressItem = $('<li>').attr('class', 'searchAddress').html(address);
+    //append the address to the container
     addressItem.appendTo($container);
 
     let phoneItem = $('<li>').attr('class', 'searchPhone').html(phone);
+    //append the phone to the container
     phoneItem.appendTo($container);
 
     let websiteItem = $('<li>').attr('class', 'searchWebsite').html(website);
+    //create a button that can link the user to the website
     let websiteButton = $('<button>').attr('class', 'websiteButton').html('Go To Website');
+    //append the website to the container
     $container.append(websiteItem);
+    //append the button to the container
     $container.append(websiteButton);
+    //wrap the button in the link
     websiteButton.wrap('<a href="'+ website +'"></a>')
-
+    //append the container to the search results
     $('#searchResults').append($container)
 
   }
